30cca9047fa95ac46ad3a93bff1fd6b4a6e4acf4
XuChao <xutongle@gmail.com>
2017-08-23 16:31:26 +0800

    init

--- common/config/main.php
+++ common/config/main.php
@@ -2,9 +2,35 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'charset' => 'utf8',
+            'tablePrefix' => 'yun_',
+        ],
+        'formatter' => [
+            'class' => 'yii\i18n\Formatter',
+            'dateFormat' => 'php:Y-M-d',
+            'datetimeFormat' => 'php:Y-m-d H:i:s',
+            'timeFormat' => 'php:H:i:s',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection'
+        ],
+        'i18n' => [
+            'translations' => [
+                'admin' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@yuncms/admin/messages',
+                ],
+                'user' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@yuncms/user/messages',
+                ],
+            ]
+        ]
     ],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
