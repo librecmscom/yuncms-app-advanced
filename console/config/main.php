12b1d5780858b56cfc7d9c37df1a4c4118b21be9
Chao Xu <xutongle@gmail.com>
2017-08-28 14:36:31 +0800

    模板

--- console/config/main.php
+++ console/config/main.php
@@ -15,7 +15,36 @@ return [
         'fixture' => [
             'class' => 'yii\console\controllers\FixtureController',
             'namespace' => 'common\fixtures',
-          ],
+        ],
+        'migrate' => [//迁移配置
+            'class' => 'yii\console\controllers\MigrateController',
+            'templateFile' => '@app/views/migration.php',
+            'interactive' => 0,//自动应答
+            'migrationNamespaces' => [//命名空间
+                'app\migrations',
+                'yuncms\admin\migrations',
+                'yuncms\system\migrations',
+                'yuncms\tag\migrations',
+                'yuncms\user\migrations',
+                'yuncms\question\migrations',
+                'yuncms\comment\migrations',
+                'yuncms\payment\migrations',
+                'yuncms\attachment\migrations',
+                'yuncms\article\migrations',
+                'yuncms\note\migrations',
+                'yuncms\message\migrations',
+                'yuncms\oauth2\migrations',
+                'yuncms\credit\migrations',
+                'yuncms\doing\migrations',
+                'yuncms\authentication\migrations',
+                'yuncms\support\migrations',
+                'yuncms\attention\migrations',
+                'yuncms\collection\migrations',
+                'yuncms\notification\migrations',
+            ],
+            // 完全禁用非命名空间迁移
+            //'migrationPath' => null,
+        ],
     ],
     'components' => [
         'log' => [
---
+++ console/views/migration.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This view is used by console/controllers/MigrateController.php
+ * The following variables are available in this view:
+ */
+/* @var $className string the new migration class name without namespace */
+/* @var $namespace string the new migration class namespace */
+
+echo "<?php\n";
+if (!empty($namespace)) {
+    echo "\nnamespace {$namespace};\n";
+}
+?>
+
+use yii\db\Migration;
+
+class <?= $className ?> extends Migration
+{
+
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+    }
+
+    public function safeDown()
+    {
+        //$this->dropTable('');
+    }
+
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "<?= $className ?> cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
