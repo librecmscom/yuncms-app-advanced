2776d36ffb516ff625de627875a666576093ca76
Chao Xu <xutongle@gmail.com>
2017-08-28 11:22:04 +0800

    a

---
+++ Ide.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Yii bootstrap file.
+ * Used for enhanced IDE code autocompletion.
+ */
+class Yii extends \yii\BaseYii
+{
+    /**
+     * @var BaseApplication|WebApplication|ConsoleApplication|WeixinApplication the application instance
+     */
+    public static $app;
+}
+
+/**
+ * Class BaseApplication
+ * Used for properties that are identical for both WebApplication and ConsoleApplication
+ *
+ * @property yuncms\system\components\Settings $settings The settings component. This property is read-only. Extended component.
+ * @property yii\queue\cli\Queue $queue The queue component. This property is read-only. Extended component.
+ */
+abstract class BaseApplication extends yii\base\Application
+{
+}
+
+/**
+ * Class WebApplication
+ * Include only Web application related components here
+ *
+ * @property \app\components\User $user The user component. This property is read-only. Extended component.
+ * @property \app\components\MyResponse $response The response component. This property is read-only. Extended component.
+ * @property \app\components\ErrorHandler $errorHandler The error handler application component. This property is read-only. Extended component.
+ */
+class WebApplication extends yii\web\Application
+{
+}
+
+/**
+ * Class ConsoleApplication
+ * Include only Console application related components here
+ *
+ * @property \app\components\ConsoleUser $user The user component. This property is read-only. Extended component.
+ */
+class ConsoleApplication extends yii\console\Application
+{
+}
+
+/**
+ * Class WeixinApplication
+ * Include only Console application related components here
+ *
+ * @property \app\components\ConsoleUser $user The user component. This property is read-only. Extended component.
+ */
+class WeixinApplication extends yii\web\Application
+{
+}
