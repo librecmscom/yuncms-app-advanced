9cb516549e0eb909cb261aa1fd65754f9d3cfb2b
Chao Xu <xutongle@gmail.com>
2017-08-29 13:51:08 +0800

    update

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -14,11 +14,76 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'panels' => [
+//            'mongodb' => [
+//                'class' => 'yii\mongodb\debug\MongoDbPanel',
+//                // 'db' => 'mongodb', // MongoDB component ID, defaults to `db`. Uncomment and change this line, if you registered MongoDB component with a different ID.
+//            ],
+            'httpclient' => [
+                'class' => 'yii\httpclient\debug\HttpClientPanel',
+            ],
+        ],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'generators' => [
+            'crud' => [ //生成器名称
+                'class' => 'yii\gii\generators\crud\Generator',
+                'template' => 'Inspinia',
+                'templates' => [ //设置我们自己的模板
+                    //模板名 => 模板路径
+                    'Inspinia' => '@vendor/xutl/yii2-inspinia-widget/gii/generators/crud',
+                ]
+            ],
+            'model' => [ //生成器名称
+                'class' => 'yii\gii\generators\model\Generator',
+                'template' => 'Inspinia',
+                'templates' => [ //设置我们自己的模板
+                    //模板名 => 模板路径
+                    'Inspinia' => '@vendor/xutl/yii2-inspinia-widget/gii/generators/model',
+                ]
+            ],
+            'module' => [ //生成器名称
+                'class' => 'yii\gii\generators\module\Generator',
+                'template' => 'Inspinia',
+                'templates' => [ //设置我们自己的模板
+                    //模板名 => 模板路径
+                    'Inspinia' => '@vendor/xutl/yii2-inspinia-widget/gii/generators/module',
+                ]
+            ],
+            'controller' => [ //生成器名称
+                'class' => 'yii\gii\generators\controller\Generator',
+                'template' => 'Inspinia',
+                'templates' => [ //设置我们自己的模板
+                    //模板名 => 模板路径
+                    'Inspinia' => '@vendor/xutl/yii2-inspinia-widget/gii/generators/module',
+                ]
+            ],
+            'extension' => [ //生成器名称
+                'class' => 'yii\gii\generators\extension\Generator',
+                'template' => 'Inspinia',
+                'templates' => [ //设置我们自己的模板
+                    //模板名 => 模板路径
+                    'Inspinia' => '@vendor/xutl/yii2-inspinia-widget/gii/generators/extension',
+                ]
+            ],
+            'form' => [ //生成器名称
+                'class' => 'yii\gii\generators\form\Generator',
+                'template' => 'Inspinia',
+                'templates' => [ //设置我们自己的模板
+                    //模板名 => 模板路径
+                    'SmartAdmin' => '@vendor/xutl/yii2-inspinia-widget/gii/generators/form',
+                ]
+            ],
+//            'sphinxModel' => [
+//                'class' => 'yii\sphinx\gii\model\Generator'
+//            ],
+//            'mongoDbModel' => [
+//                'class' => 'yii\mongodb\gii\model\Generator'
+//            ],
+        ]
     ];
 }
 
