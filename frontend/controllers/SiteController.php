57b787f9b77730f28a6e5f44f9aef62cf5e752e1
Chao Xu <xutongle@gmail.com>
2017-08-30 12:29:08 +0800

    cc

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -27,6 +27,10 @@ class SiteController extends Controller
             'error' => [
                 'class' => 'yii\web\ErrorAction',
             ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
         ];
     }
 
