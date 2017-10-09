d497c337b3c769d35e9c572182822f93d2d13040
Peter <xutongle@gmail.com>
2017-10-09 17:33:09 +0800

    支付

---
+++ api/modules/v1/controllers/PaymentController.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace api\modules\v1\controllers;
+
+use api\modules\v1\ActiveController;
+
+/**
+ * Class PaymentController
+ * @package api\modules\v1\controllers
+ */
+class PaymentController extends ActiveController
+{
+    public function actionUnifiedOrder()
+    {
+
+    }
+
+    public function actionOrderQuery()
+    {
+
+    }
+
+    public function actionCloseOrder()
+    {
+
+    }
+
+    public function actionRefund()
+    {
+
+    }
+
+    public function actionRefundQuery()
+    {
+
+    }
+}
