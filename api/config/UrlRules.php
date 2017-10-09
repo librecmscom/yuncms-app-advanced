15d663e5e77379e9a5b2277f46e84e50fb7e3d0d
Peter <xutongle@gmail.com>
2017-10-09 13:52:39 +0800

    update

--- api/config/UrlRules.php
+++ api/config/UrlRules.php
@@ -4,45 +4,11 @@
  * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
  * @license http://www.tintsoft.com/license/
  */
-return [
-    'GET /' => 'site/index',
-    'GET ping' => 'site/ping',
-    'GET location' => 'site/location',
-
-    //第一版
-    [//公共接口
-        'class' => 'yii\rest\UrlRule',
-        'except' => ['delete', 'create', 'update'],
-        'controller' => ['v1/category', 'v1/language', 'v1/area', 'v1/country', 'v1/currency']
+return array_merge(
+    [
+        'GET /' => 'site/index',
+        'GET ping' => 'site/ping',
+        'GET location' => 'site/location',
     ],
-    [//话题
-        'class' => 'yii\rest\UrlRule',
-        'except' => ['delete', 'create', 'update'],
-        'controller' => 'v1/topic',
-        'extraPatterns' => [
-            'GET search' => 'search',
-        ],
-    ],
-    [//文章
-        'class' => 'yii\rest\UrlRule',
-        'controller' => [
-            'v1/article',
-        ]
-    ],
-    [//用户
-        'class' => 'yii\rest\UrlRule',
-        'controller' => 'v1/user',
-        'except' => ['delete', 'create'],
-        'extraPatterns' => [
-            'GET search' => 'search',
-            'POST avatar' => 'avatar',
-            'GET post' => 'register',
-        ],
-//        'ruleConfig' => [//额外的包含规则
-//            'class' => 'yii\web\UrlRule',
-//            'defaults' => [
-//                'expand' => 'profile',
-//            ]
-//        ],
-    ],
-];
+    require(__DIR__ . '/../modules/v1/router.php')
+);
---
+++ api/modules/v1/router.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+ return [
+     //第一版
+     [//公共接口
+         'class' => 'yii\rest\UrlRule',
+         'except' => ['delete', 'create', 'update'],
+         'controller' => ['v1/category', 'v1/language', 'v1/area', 'v1/country', 'v1/currency']
+     ],
+     [//话题
+         'class' => 'yii\rest\UrlRule',
+         'except' => ['delete', 'create', 'update'],
+         'controller' => 'v1/topic',
+         'extraPatterns' => [
+             'GET search' => 'search',
+         ],
+     ],
+     [//文章
+         'class' => 'yii\rest\UrlRule',
+         'controller' => [
+             'v1/article',
+         ]
+     ],
+     [//用户
+         'class' => 'yii\rest\UrlRule',
+         'controller' => 'v1/user',
+         'except' => ['delete', 'create'],
+         'extraPatterns' => [
+             'GET search' => 'search',
+             'POST avatar' => 'avatar',
+             'GET post' => 'register',
+         ],
+//        'ruleConfig' => [//额外的包含规则
+//            'class' => 'yii\web\UrlRule',
+//            'defaults' => [
+//                'expand' => 'profile',
+//            ]
+//        ],
+     ],
+ ];
